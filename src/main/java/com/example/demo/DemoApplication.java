b1ecb74c8d9b6e70f326310b06133eb0f08b01a4
keshavkumar-web <keshav.kumar@vinbox.in>
2026-01-11 11:46:53 +0000

    Fix Spring Boot imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,12 @@
-@RestController
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     @GetMapping("/")
